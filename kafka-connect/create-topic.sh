700d4bb6ffb274052831c2c4fd8e6f62f45e83ef
talsii <talsi.uk@gmail.com>
2021-03-01 20:25:27 +0100

    mirror

--- kafka-connect/create-topic.sh
+++ kafka-connect/create-topic.sh
@@ -2,5 +2,9 @@
 
 export bootstrap_servers="b-2.msk-eu-west-1.15x4ul.c1.kafka.eu-west-1.amazonaws.com:9092,b-3.msk-eu-west-1.15x4ul.c1.kafka.eu-west-1.amazonaws.com:9092,b-1.msk-eu-west-1.15x4ul.c1.kafka.eu-west-1.amazonaws.com:9092"
 
-kafka-topics --bootstrap-server ${bootstrap_servers} --create --topic connect_file --partitions 10 --replication-factor 3
+topic=${1}
+partitions=${2}
+replication_factor=${3}
+echo "Creating topic ${topic} with ${partitions} and a replication factor of ${replication_factor} .."
+#kafka-topics --bootstrap-server ${bootstrap_servers} --create --topic connect_file --partitions 10 --replication-factor 3
 
