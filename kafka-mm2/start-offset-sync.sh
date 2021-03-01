cb68c78678c51643a5c94b7be310882db5c75c68
talsii <talsi.uk@gmail.com>
2021-03-01 16:08:10 +0100

    mirror

--- kafka-mm2/start-offset-sync.sh
+++ kafka-mm2/start-offset-sync.sh
@@ -3,4 +3,4 @@
 java -jar   /tmp/kafka/MM2GroupOffsetSync-1.0-SNAPSHOT.jar 
             -cgi mm2TestConsumer1 \
             -src msksource \
-            -pfp /tmp/kafka/consumer.properties_sync_dest 2>&1 > /dev/null &
+            -pfp /tmp/kafka/consumer.properties 2>&1 > /dev/null &
