27ede720520a1ca4c5cf995d1eb67730046f04a3
talsii <talsi.uk@gmail.com>
2021-03-01 20:42:22 +0100

    mirror

--- kafka-connect/create-s3-sink-connect.sh
+++ kafka-connect/create-s3-sink-connect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-body="'"{
+body="{
     \"name\": \"s3_sink_connector\",
     \"config\": {
         \"connector.class\": \"io.confluent.connect.s3.S3SinkConnector\",
@@ -14,7 +14,7 @@ body="'"{
         \"partitioner.class\": \"io.confluent.connect.storage.partitioner.DefaultPartitioner\",
         \"storage.class\": \"io.confluent.connect.s3.storage.S3Storage\"
     }
-}"'"
+}"
 
 curl -X POST http://10.10.1.26:8083/connectors \
 -H 'Accept: */*' \
