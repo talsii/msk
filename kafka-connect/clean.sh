9c59a572f0e91d4f86d62b1ba3d09c3ba6b13589
talsii <talsi.uk@gmail.com>
2021-03-01 20:59:51 +0100

    mirror

--- kafka-connect/clean.sh
+++ kafka-connect/clean.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export bootstrap_servers="b-2.msk-eu-west-1.15x4ul.c1.kafka.eu-west-1.amazonaws.com:9092,b-3.msk-eu-west-1.15x4ul.c1.kafka.eu-west-1.amazonaws.com:9092,b-1.msk-eu-west-1.15x4ul.c1.kafka.eu-west-1.amazonaws.com:9092"
+
 topic=${topic}
 
 echo "Deleting connectors ..."
@@ -10,4 +12,4 @@ curl -X DELETE http://10.10.1.26:8083/connectors/s3_source_connector
 echo "Deleting topic"
 
 echo "Checking created topic ${topic} ..."
-kafka-topics --bootstrap-server ${bootstrap_servers} --describe --topic ${topic}
+kafka-topics --bootstrap-server ${bootstrap_servers} --delete --topic ${topic}
