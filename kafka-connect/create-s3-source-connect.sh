5e98e4bf901d5900833d337a465dd3df8a672dca
talsii <talsi.uk@gmail.com>
2021-03-01 21:23:27 +0100

    mirror

--- kafka-connect/create-s3-source-connect.sh
+++ kafka-connect/create-s3-source-connect.sh
@@ -8,13 +8,12 @@ cat <<EOF
 {
     "name": "${connector}",
     "config": {
-        "connector.class": "io.confluent.connect.s3.S3SourceConnector",
+        "connector.class": "io.confluent.connect.s3.source.S3SourceConnector",
         "topics": "${topic}",
         "s3.region": "eu-west-1",
+        "tasks.max": 1,
         "s3.bucket.name": "talsii.kafka.connect.source",
         "s3.part.size": "5242880",
-        "flush.size": 3,
-        "value.converter": "org.apache.kafka.connect.storage.StringConverter",
         "format.class": "io.confluent.connect.s3.format.avro.AvroFormat",
         "partitioner.class": "io.confluent.connect.storage.partitioner.DefaultPartitioner",
         "storage.class": "io.confluent.connect.s3.storage.S3Storage"
