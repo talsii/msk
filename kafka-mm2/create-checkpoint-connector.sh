f1fa5597d8896a9b10b2bcc94018a72495986050
talsii <talsi.uk@gmail.com>
2021-03-01 15:33:21 +0100

    mirror

--- kafka-mm2/create-checkpoint-connector.sh
+++ kafka-mm2/create-checkpoint-connector.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Creating MirrorCheckpointConnector ..."
-curl -X PUT -H "Content-Type: application/json" --data @/tmp/kafka/mm2-cpc.json http://localhost:8083/connectors/mm2-msc/config
+curl -X PUT -H "Content-Type: application/json" --data @/tmp/kafka/mm2-cpc.json http://localhost:8083/connectors/mm2-cpc/config
 
 echo "Checking MirrorCheckpointConnector status ..."
 curl -s http://localhost:8083/connectors/mm2-cpc/status | jq .
--- kafka-mm2/create-heartbeat-connector.sh
+++ kafka-mm2/create-heartbeat-connector.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Creating MirrorCheckpointConnector ..."
-curl -X PUT -H "Content-Type: application/json" --data @/tmp/kafka/mm2-hbc.json http://localhost:8083/connectors/mm2-msc/config
+curl -X PUT -H "Content-Type: application/json" --data @/tmp/kafka/mm2-hbc.json http://localhost:8083/connectors/mm2-hbc/config
 
 echo "Checking MirrorCheckpointConnector status ..."
 curl -s http://localhost:8083/connectors/mm2-hbc/status | jq .
