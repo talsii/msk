4d3de19a4785cf8f9f2b2d9de2545c8aedd3d68a
talsii <talsi.uk@gmail.com>
2021-03-01 20:46:03 +0100

    mirror

--- kafka-connect/create-s3-sink-connect.sh
+++ kafka-connect/create-s3-sink-connect.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-body="{
-    \"name\": \"s3_sink_connector\",
-    \"config\": {
-        \"connector.class\": \"io.confluent.connect.s3.S3SinkConnector\",
-        \"topics\": \"${topic}\",
-        \"s3.region\": \"eu-west-1\",
-        \"s3.bucket.name\": \"talsii.kafka.connect\",
-        \"s3.part.size\": \"5242880\",
-        \"flush.size\": 3,
-        \"value.converter\": \"org.apache.kafka.connect.storage.StringConverter\",
-        \"format.class\": \"io.confluent.connect.s3.format.avro.AvroFormat\",
-        \"partitioner.class\": \"io.confluent.connect.storage.partitioner.DefaultPartitioner\",
-        \"storage.class\": \"io.confluent.connect.s3.storage.S3Storage\"
+topic=${1}
+
+generate_body() {
+cat <<EOF
+{
+    "name": "s3_sink_connector",
+    "config": {
+        "connector.class": "io.confluent.connect.s3.S3SinkConnector",
+        "topics": "${topic}",
+        "s3.region": "eu-west-1",
+        "s3.bucket.name": "talsii.kafka.connect",
+        "s3.part.size": "5242880",
+        "flush.size": 3,
+        "value.converter": "org.apache.kafka.connect.storage.StringConverter",
+        "format.class": "io.confluent.connect.s3.format.avro.AvroFormat",
+        "partitioner.class": "io.confluent.connect.storage.partitioner.DefaultPartitioner",
+        "storage.class": "io.confluent.connect.s3.storage.S3Storage"
     }
-}"
+}
+EOF
+}
 
 curl -X POST http://10.10.1.26:8083/connectors \
 -H 'Accept: */*' \
 -H 'Content-Type: application/json' \
--d ${body}
+-d "$(generate_body)"
